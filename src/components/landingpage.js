836a1b0eef2718e40e5796aa2def036247ef2697
Andhika H. Santoso <andhika.haeruman@andhikahs.tech>
2019-02-28 15:29:43 +0700

    Add basic page for staging server testing

---
+++ src/components/landingpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    render() {
+        return (
+            <div style={{width: '100%', margin: 'auto'}}>
+            
+            </div>
+        );
+    }
+}
+
+export default Landing;
